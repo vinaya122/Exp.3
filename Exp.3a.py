ad1d1178cdc5a3fe5383f42a9d4317d79b575699
vinaya122 <kadamvinaya122@gmail.com>
2026-04-29 13:25:40 +0530

    Add files via upload

---
+++ Exp.3a.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 11:12:29 2026
+
+@author: Agce
+"""
+
+n = int(input("Enter number of rows: ")) 
+
+
+
+for i in range(1, n + 1): 
+    
+  for j in range(1, i + 1):
+      
+    print(j, end=" ")
+    
+print()
